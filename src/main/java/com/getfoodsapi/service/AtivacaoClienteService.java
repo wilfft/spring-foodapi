5dddc01362b1ef045307d6858f673bcaab869376
wilfft <68745408+wilfft@users.noreply.github.com>
2021-02-16 16:05:59 -0300

    refact: now is using  qualifier to call components

--- src/main/java/com/getfoodsapi/service/AtivacaoClienteService.java
+++ src/main/java/com/getfoodsapi/service/AtivacaoClienteService.java
@@ -1,6 +1,7 @@
 package com.getfoodsapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.getfoodsapi.modelos.Cliente;
@@ -9,19 +10,18 @@ import com.getfoodsapi.notificacao.Notificador;
 @Component
 public class AtivacaoClienteService {
 
-@Autowired
-    private    Notificador notificador;
-
+    @Qualifier("urgente")
+    @Autowired
+    private Notificador notificador;
 
     public void Ativar(Cliente cliente) {
 	cliente.ativar();
 	if (notificador != null) {
-		notificador.notificar(cliente,
-				cliente.getEmail() + "  foi notificado via ativaçao cliente.  " + cliente.toString());
+	    notificador.notificar(cliente, cliente.toString().toUpperCase());
 
 	} else {
 
-		System.out.println("Cliente nao notificado ");
+	    System.out.println("Cliente nao notificado ");
 	}
     }
 }
