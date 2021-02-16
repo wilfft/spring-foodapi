8eeb31750cd51ccec982a306b43739928470d380
wilfft <68745408+wilfft@users.noreply.github.com>
2021-02-16 15:44:50 -0300

    refact: using @Primary to call a only one component

--- src/main/java/com/getfoodsapi/notificacao/NotificadorEmail.java
+++ src/main/java/com/getfoodsapi/notificacao/NotificadorEmail.java
@@ -1,13 +1,15 @@
 package com.getfoodsapi.notificacao;
 
 import com.getfoodsapi.modelos.Cliente;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Primary
+ @Component
 public class NotificadorEmail implements Notificador {
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-	System.out.println(cliente.getNome() + " foi notificado com a mensagem: " + mensagem);
+	System.out.println(cliente.getNome() + "  notificado por email: " + cliente.getEmail());
     }
 }
